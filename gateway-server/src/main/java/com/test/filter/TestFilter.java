609235b987eac3e12d5247903152ccfbcc869960
aileen <ailing.deng@thoughtworks.com>
2022-07-25 17:54:18 +0800

    feat: make TestFilter have the highest priority.

--- gateway-server/src/main/java/com/test/filter/TestFilter.java
+++ gateway-server/src/main/java/com/test/filter/TestFilter.java
@@ -2,6 +2,7 @@ package com.test.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,16 +11,20 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Component
-public class TestFilter implements GlobalFilter {
+public class TestFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        System.out.println(request.getQueryParams());
         List<String> stringList = request.getQueryParams().get("test");
         if (stringList != null && stringList.contains("1")) {
             return chain.filter(exchange);
         } else {
             return exchange.getResponse().setComplete();
         }
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
