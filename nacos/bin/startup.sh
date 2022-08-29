bbec132bb2be72fbafd5fc120aacbde27782d7c3
aileen <ailing.deng@thoughtworks.com>
2022-08-29 16:08:07 +0800

    feat: register services in nacos.

--- nacos/bin/startup.sh
+++ nacos/bin/startup.sh
@@ -138,5 +138,4 @@ if [ ! -f "${BASE_DIR}/logs/start.out" ]; then
 fi
 # start
 echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/start.out 2>&1 &
-nohup $JAVA ${JAVA_OPT} nacos.nacos >> ${BASE_DIR}/logs/start.out 2>&1 &
-echo "nacos is starting，you can check the ${BASE_DIR}/logs/start.out"
+$JAVA ${JAVA_OPT} nacos.nacos
