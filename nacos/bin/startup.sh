6ae798c0bed006e32af5b2d1a97042f719c9b6f6
aileen <ailing.deng@thoughtworks.com>
2022-09-02 15:10:32 +0800

    feat: create nacos database and config two nacos servers.

--- nacos/bin/startup.sh
+++ nacos/bin/startup.sh
@@ -90,7 +90,7 @@ else
     if [[ "${EMBEDDED_STORAGE}" == "embedded" ]]; then
         JAVA_OPT="${JAVA_OPT} -DembeddedStorage=true"
     fi
-    JAVA_OPT="${JAVA_OPT} -server -Xms2g -Xmx2g -Xmn1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m"
+    JAVA_OPT="${JAVA_OPT} -server -Xms512m -Xmx512m -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m"
     JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
     JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
 
