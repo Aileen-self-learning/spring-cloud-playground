20fcbe728980759ffaf7942bfd5e0473f335a2e7
aileen <ailing.deng@thoughtworks.com>
2022-09-02 13:47:53 +0800

    feat: fetch config value with @Value and print it in UserController.

--- user-service/src/main/java/com/test/controller/UserController.java
+++ user-service/src/main/java/com/test/controller/UserController.java
@@ -2,6 +2,7 @@ package com.test.controller;
 
 import com.test.entity.User;
 import com.test.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,15 @@ import javax.annotation.Resource;
 
 @RestController
 public class UserController {
-
     @Resource
     UserService service;
 
+    @Value("${test.lb}")
+    String lb;
+
     @GetMapping("/user/{uuid}")
     public User findUserById(@PathVariable("uuid") int uuid) {
+        System.out.println("lb: " + lb);
         return service.getUserById(uuid);
     }
 }
